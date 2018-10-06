b661fb8bcc25f763d6560d78d3b1dae6323e0e8b
nelcifran <nelcifranpires@gmail.com>
2018-10-06 18:39:19 -0300

    model de user, fix em events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :address, :duration_time, :max_people, :ticket_value,:,:file_appeal :persona_id)
+      params.require(:event).permit(:name, :address, :duration_time, :max_people, :ticket_value,:file_appeal, :user_id)
     end
 end
